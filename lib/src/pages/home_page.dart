40317d33c1860d1dcfbee2e5f220f14a57fe49eb
goandreus <andreshtml5@gmail.com>
2019-09-03 09:35:53 -0500

    swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hola mundo!!!!!'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.orangeAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarget()
+          ],
+        ),
       )
       
        
+    );
+  }
+
+   Widget _swiperTarget(){
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context, int index){
+          return Image.network("https://images.pexels.com/photos/2007401/pexels-photo-2007401.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          fit: BoxFit.fill,);
+        },
+        itemCount: 3,
+        //pagination: SwiperPagination(),
+        //control: SwiperControl(),
+      ),
     );
   }
 }
