37c933bc4ea02134bc645a364b53c70d4791c047
goandreus <andreshtml5@gmail.com>
2019-10-04 15:54:21 -0500

    strambuilder

--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -3,19 +3,32 @@ import 'package:project/src/models/pelicula_model.dart';
 
 class MovieHorizontal extends StatelessWidget {
   final List<Pelicula> peliculas;
-  MovieHorizontal({@required this.peliculas});
-  
+  final Function siguientePagina;
+
+  MovieHorizontal({@required this.peliculas, @required this.siguientePagina});
+
+  final _pageController = PageController(
+    initialPage: 1, 
+    viewportFraction: 0.3
+  );
+
   @override
   Widget build(BuildContext context) {
 
     final _screenSize = MediaQuery.of(context).size;
+    _pageController.addListener((){
+      if (_pageController.position.pixels >= _pageController.position.maxScrollExtent -200){
+        siguientePagina();
+      }
+    });
 
 
     return Container(
-      height: _screenSize.height * 0.2,
+      height: _screenSize.height * 0.21,
       child: PageView(
         pageSnapping: false,
-        controller: PageController(initialPage: 1, viewportFraction: 0.3),
+        controller: _pageController,
+        
         children: _tarjetas(context),
       ),
     );
